3fdcd0c9e7f431042203d52bb34504466e40adbe
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 07:09:40 +0000

    [automatic commit] 2025-06-21 07:09:39 UTC ;; communications error to 208.67.222.222#53: timed out 122.199.51.39

--- part1/routes/apis.js
+++ part1/routes/apis.js
@@ -5,8 +5,8 @@ router.get('/dogs', async function(req, res) {
   try {
     db.execute(`
       SELECT Dogs.name, Dogs.size, User.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id
-    `).then((res) => {
-      JSON.dumps()
+    `).then((result) => {
+      res.json(result);
     })
   } catch (err) {
     console.error('Error querying', err);
