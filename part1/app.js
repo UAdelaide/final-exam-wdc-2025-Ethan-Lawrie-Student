6884b4f00e14df4ba1b66d073c0ea866a189a6e8
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 06:56:11 +0000

    [automatic commit] 2025-06-21 06:56:10 UTC 122.199.51.39

--- part1/app.js
+++ part1/app.js
@@ -28,7 +28,8 @@ let db;
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF EXISTS DogWalkService; CREATE DATABASE DogWalkService;');
+    await connection.query('DROP DATABASE IF EXISTS DogWalkService; CREATE DATABASE DogWalkService;');
+
     await connection.end();
 
     // Now connect to the created database
