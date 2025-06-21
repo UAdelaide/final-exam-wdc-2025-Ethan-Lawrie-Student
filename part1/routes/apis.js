e770ca29ab9e5a1114c61559f93041fae3c347ce
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 07:06:42 +0000

    [automatic commit] 2025-06-21 07:06:42 UTC 122.199.51.39

--- part1/routes/apis.js
+++ part1/routes/apis.js
@@ -4,14 +4,10 @@ var router = express.Router();
 router.get('/dogs', async function(req, res) {
   try {
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS books (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255),
-        author VARCHAR(255)
-      )
+      SELECT name, size, 
     `);
   } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+    console.error('Error querying', err);
   }
 });
 
