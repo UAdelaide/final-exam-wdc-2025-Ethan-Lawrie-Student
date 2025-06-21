ada17c6e425ba90c996368d9e9157e374af77123
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 07:33:07 +0000

    [automatic commit] 2025-06-21 07:33:07 UTC 122.199.51.39

--- part1/routes/apis.js
+++ part1/routes/apis.js
@@ -33,7 +33,7 @@ router.get('/walkrequests/open', async function(req, res) {
 router.get('/walkers/summary', async function(req, res) {
   try {
     req.db.execute(`
-      SELECT Users.username AS 'walker_username', COUNT(*) AS 'total_ratings', AVG(WalkRatings.rating) AS average_rating, COUNT(WalkRequests.status = 'completed') FROM WalkRequests INNER JOIN 
+      SELECT Users.username AS 'walker_username', COUNT(*) AS 'total_ratings', AVG(WalkRatings.rating) AS average_rating, COUNT(WalkRequests.status = 'completed') FROM WalkRequests INNER JOIN
     `).then((result) => {
       res.json(result);
     })
