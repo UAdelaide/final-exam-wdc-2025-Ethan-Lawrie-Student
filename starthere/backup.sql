cbdbeb7abdf5a8102efb972e9c2841a2891bf8fc
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 01:32:14 +0000

    [automatic commit] 2025-06-21 01:32:13 UTC 122.199.51.39

---
+++ starthere/backup.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: TextbookSelling
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.22.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Book`
+--
+
+DROP TABLE IF EXISTS `Book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Book` (
+  `book_id` int NOT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `ISBN` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Book`
+--
+
+LOCK TABLES `Book` WRITE;
+/*!40000 ALTER TABLE `Book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ListedBook`
+--
+
+DROP TABLE IF EXISTS `ListedBook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ListedBook` (
+  `listing_id` int NOT NULL AUTO_INCREMENT,
+  `listing_status` tinyint(1) DEFAULT NULL,
+  `seller_id` varchar(50) DEFAULT NULL,
+  `book_id` int DEFAULT NULL,
+  PRIMARY KEY (`listing_id`),
+  KEY `seller_id` (`seller_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `ListedBook_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `User` (`student_id`),
+  CONSTRAINT `ListedBook_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `Book` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ListedBook`
+--
+
+LOCK TABLES `ListedBook` WRITE;
+/*!40000 ALTER TABLE `ListedBook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ListedBook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `TransactionHistory`
+--
+
+DROP TABLE IF EXISTS `TransactionHistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `TransactionHistory` (
+  `transaction_id` int NOT NULL,
+  `listing_id` int DEFAULT NULL,
+  `buyer_id` varchar(50) DEFAULT NULL,
+  `transaction_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `buyer_id` (`buyer_id`),
+  KEY `listing_id` (`listing_id`),
+  CONSTRAINT `TransactionHistory_ibfk_1` FOREIGN KEY (`buyer_id`) REFERENCES `User` (`student_id`),
+  CONSTRAINT `TransactionHistory_ibfk_2` FOREIGN KEY (`listing_id`) REFERENCES `ListedBook` (`listing_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `TransactionHistory`
+--
+
+LOCK TABLES `TransactionHistory` WRITE;
+/*!40000 ALTER TABLE `TransactionHistory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TransactionHistory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `student_id` varchar(50) NOT NULL,
+  `location` varchar(200) DEFAULT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-21  1:31:59
