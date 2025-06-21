c8e1cabf0b9a23cd74b01c798431323ee8a6b658
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 04:29:17 +0000

    [automatic commit] 2025-06-21 04:29:16 UTC 122.199.51.39

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -32,7 +32,7 @@ router.get('/getListings', function(req, res) {
 router.post('/postMessage', function(req, res) {
 
   const {buyer_id, seller_id, message} = req.body;
-  console.log("buyer id: ", seller_id, "");
+  console.log("buyer id: ", seller_id, ", msg: ", message);
 
   req.pool.getConnection(function(error, connection) {
     if(error) {
