ed02bce23d2c6f5d21ee3c8f1202907d4e684043
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 06:28:44 +0000

    [automatic commit] 2025-06-21 06:28:44 UTC 122.199.51.39

--- part1/queries.sql
+++ part1/queries.sql
@@ -19,4 +19,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES (
 
 
 
-INSERT INTO Dogs (owner_id, name, size) (SELECT user_id FROM Users WHERE username = "")
+INSERT INTO Dogs (owner_id, name, size) (SELECT user_id FROM Users WHERE username = "alice123", "Max", )
