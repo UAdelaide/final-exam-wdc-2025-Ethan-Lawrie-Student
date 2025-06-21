8ffcac875a1f44873dd734befff3a10818ed09f7
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 06:58:59 +0000

    [automatic commit] 2025-06-21 06:58:59 UTC 122.199.51.39

--- part1/app.js
+++ part1/app.js
@@ -101,45 +101,45 @@ let db;
     // const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES (
-            "alice123", "alice@example.com", "hashed123", 'owner'
+            'alice123', 'alice@example.com', 'hashed123', 'owner'
         );
         INSERT INTO Users (username, email, password_hash, role) VALUES (
-            "bobwalker", "bob@example.com", "hashed456", 'walker'
-        );
-
-        INSERT INTO Users (username, email, password_hash, role) VALUES (
-            "carol123", "carol@example.com", "hashed789", 'owner'
+            'bobwalker', 'bob@example.com', 'hashed456', 'walker'
         );
 
         INSERT INTO Users (username, email, password_hash, role) VALUES (
-            "ethanL", "ethan@example.com", "hashed111", 'owner'
+            'carol123', 'carol@example.com', 'hashed789', 'owner'
         );
 
         INSERT INTO Users (username, email, password_hash, role) VALUES (
-            'jerry', 'jerry@example.com', "hashedJerry", 'walker'
+            'ethanL', 'ethan@example.com', 'hashed111', 'owner'
+        );
+
+        INSERT INTO Users (username, email, password_hash, role) VALUES (
+            'jerry', 'jerry@example.com', 'hashedJerry', 'walker'
         );
 
 
 
-        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Max', 'medium' FROM Users WHERE username = "alice123");
+        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Max', 'medium' FROM Users WHERE username = 'alice123');
 
 
 
-        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Bella', 'small' FROM Users WHERE username = "carol123");
+        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Bella', 'small' FROM Users WHERE username = 'carol123');
 
 
 
-        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'EthanJnr', 'small' FROM Users WHERE username = "ethanL");
+        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'EthanJnr', 'small' FROM Users WHERE username = 'ethanL');
 
 
 
-        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Ruff', 'small' FROM Users WHERE username = "ethanL");
+        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Ruff', 'small' FROM Users WHERE username = 'ethanL');
 
 
-        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Bluey', 'medium' FROM Users WHERE username = "ethanL");
+        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Bluey', 'medium' FROM Users WHERE username = 'ethanL');
 
 
-        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Fred', 'large' FROM Users WHERE username = "alice123");
+        INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , 'Fred', 'large' FROM Users WHERE username = 'alice123');
 
 
 
