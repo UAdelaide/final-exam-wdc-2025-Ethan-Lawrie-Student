98ce9dbeb282dcaa0bd5b8ba12844991670565a7
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 04:09:43 +0000

    [automatic commit] 2025-06-21 04:09:43 UTC 122.199.51.39

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -27,4 +27,26 @@ router.get('/getListings', function(req, res) {
 });
 
 
+
+
+router.get('/getListings', function(req, res) {
+  req.pool.getConnection(function(error, connection) {
+    if(error) {
+      res.status(500).send("Error connecting ");
+      return;
+    }
+
+
+
+    connection.query('SELECT User.full_name, User.student_id, Book.title, Book.author, ListedBook.listing_id, ListedBook.sell_price FROM ListedBook INNER JOIN Book ON ListedBook.book_id = Book.book_id INNER JOIN User ON ListedBook.seller_id = User.student_id WHERE ListedBook.listing_status = TRUE', function(errror2, rows) {
+      if(error) {
+          res.status(500).send("Error querying");
+        return;
+      }
+      res.json(rows);
+    });
+  })
+});
+
+
 module.exports = router;
