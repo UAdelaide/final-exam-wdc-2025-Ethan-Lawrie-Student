7a72dff13935db8b4289674ecb3964e8cfc1d68f
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 07:28:33 +0000

    [automatic commit] 2025-06-21 07:28:32 UTC ;; communications error to 208.67.222.222#53: timed out 122.199.51.39

--- part1/routes/apis.js
+++ part1/routes/apis.js
@@ -33,7 +33,7 @@ router.get('/walkrequests/open', async function(req, res) {
 router.get('/walkers/summary', async function(req, res) {
   try {
     req.db.execute(`
-      SELECT Users.username AS 'walker_username', COUNT(*) AS 'total_ratings', AVG(WalkRatings.rating) AS average_rating
+      SELECT Users.username AS 'walker_username', COUNT(*) AS 'total_ratings', AVG(WalkRatings.rating) AS average_rating, 
     `).then((result) => {
       res.json(result);
     })
