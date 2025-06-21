15e569904a628a7547a9279c5e6c072fd1a98605
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 07:22:29 +0000

    [automatic commit] 2025-06-21 07:22:28 UTC 122.199.51.39

--- part1/routes/apis.js
+++ part1/routes/apis.js
@@ -30,4 +30,17 @@ router.get('/walkrequests/open', async function(req, res) {
 });
 
 
+router.get('/walkrequests/open', async function(req, res) {
+  try {
+    req.db.execute(`
+      SELECT WalkRequests.request_id, Dogs.name AS 'dog_name', WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS 'owner_username' FROM WalkRequests INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE WalkRequests.status = 'open'
+    `).then((result) => {
+      res.json(result);
+    })
+  } catch (err) {
+    console.error('Error querying', err);
+  }
+});
+
+
 module.exports = router;
