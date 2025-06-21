3a3f17fb6f9aefe9fae4f9bbf64f17400cd4396d
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 03:28:30 +0000

    [automatic commit] 2025-06-21 03:28:30 UTC 122.199.51.39

--- part1/app.js
+++ part1/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const app = express();
 
 const mysql = require('mysql');
 
@@ -44,5 +44,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-module.exports = app;
