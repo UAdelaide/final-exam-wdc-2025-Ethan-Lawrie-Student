88359e6e0907644cf554c3665ef432929f107c46
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 04:10:05 +0000

    [automatic commit] 2025-06-21 04:10:05 UTC 122.199.51.39

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -29,14 +29,13 @@ router.get('/getListings', function(req, res) {
 
 
 
-router.get('/getListings', function(req, res) {
+router.post('/sendMessage', function(req, res) {
+
   req.pool.getConnection(function(error, connection) {
     if(error) {
       res.status(500).send("Error connecting ");
       return;
     }
-
-
 
     connection.query('SELECT User.full_name, User.student_id, Book.title, Book.author, ListedBook.listing_id, ListedBook.sell_price FROM ListedBook INNER JOIN Book ON ListedBook.book_id = Book.book_id INNER JOIN User ON ListedBook.seller_id = User.student_id WHERE ListedBook.listing_status = TRUE', function(errror2, rows) {
       if(error) {
