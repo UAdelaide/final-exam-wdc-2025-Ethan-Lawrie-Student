f2d154916ca67e54a682776f91cc1952165b7b93
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 06:23:47 +0000

    [automatic commit] 2025-06-21 06:23:46 UTC 122.199.51.39

--- part1/queries.sql
+++ part1/queries.sql
@@ -8,3 +8,11 @@ INSERT INTO Users (username, email, password_hash, role) VALUES (
 INSERT INTO Users (username, email, password_hash, role) VALUES (
     "carol123", "carol@example.com", "hashed789", 'owner'
 );
+
+INSERT INTO Users (username, email, password_hash, role) VALUES (
+    "ethanL", "ethan@example.com", "hashed111", 'owner'
+);
+
+INSERT INTO Users (username, email, password_hash, role) VALUES (
+    "ethanL", "ethan@example.com", "hashed111", 'owner'
+);
