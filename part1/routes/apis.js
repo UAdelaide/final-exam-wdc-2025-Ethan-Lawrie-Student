e49664b9c40192f441004e53e0346cb4d6e481de
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 07:20:38 +0000

    [automatic commit] 2025-06-21 07:20:38 UTC 122.199.51.39

--- part1/routes/apis.js
+++ part1/routes/apis.js
@@ -20,7 +20,7 @@ router.get('/dogs', async function(req, res) {
 router.get('/walkrequests/open', async function(req, res) {
   try {
     req.db.execute(`
-      SELECT WalkRequests.request_id, Dogs.name AS 'dog_name', WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS 'owner_username' FROM WalkRequests INNER JOIN Dog ON WalkRequests.dog_id = Dog.dog_id INNER JOIN Users ON Users ON Dogs.owner_id = Users.user_id WHERE WalkRequests.status = 'open'
+      SELECT WalkRequests.request_id, Dogs.name AS 'dog_name', WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS 'owner_username' FROM WalkRequests INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE WalkRequests.status = 'open'
     `).then((result) => {
       res.json(result);
     })
