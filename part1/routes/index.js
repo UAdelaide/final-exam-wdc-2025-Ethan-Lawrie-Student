51f70bc52815cdd461551d4c0eb23e2c79f06b2d
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 04:14:11 +0000

    [automatic commit] 2025-06-21 04:14:10 UTC 122.199.51.39

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -16,8 +16,8 @@ router.get('/getListings', function(req, res) {
 
 
 
-    connection.query('SELECT User.full_name, User.student_id, Book.title, Book.author, ListedBook.listing_id, ListedBook.sell_price FROM ListedBook INNER JOIN Book ON ListedBook.book_id = Book.book_id INNER JOIN User ON ListedBook.seller_id = User.student_id WHERE ListedBook.listing_status = TRUE', function(errror2, rows) {
-      if(error) {
+    connection.query('SELECT User.full_name, User.student_id, Book.title, Book.author, ListedBook.listing_id, ListedBook.sell_price FROM ListedBook INNER JOIN Book ON ListedBook.book_id = Book.book_id INNER JOIN User ON ListedBook.seller_id = User.student_id WHERE ListedBook.listing_status = TRUE', function(error2, rows) {
+      if(error2) {
           res.status(500).send("Error querying");
         return;
       }
@@ -39,8 +39,8 @@ router.post('/sendMessage', function(req, res) {
       return;
     }
 
-    connection.query('INSERT INTO Message (potential_buyer_id, potential_seller_id, message), (?,?,?)', [buyer_id,seller_id, message.trim()], function(errror2, rows) {
-      if(error) {
+    connection.query('INSERT INTO Message (potential_buyer_id, potential_seller_id, message), (?,?,?)', [buyer_id,seller_id, message.trim()], function(error2, rows) {
+      if(error2) {
           res.status(500).send("Error querying");
         return;
       }
