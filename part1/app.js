b582cbc12d2b135a013face62efa93f729f5eb57
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 02:45:28 +0000

    [automatic commit] 2025-06-21 02:45:28 UTC 122.199.51.39

--- part1/app.js
+++ part1/app.js
@@ -3,8 +3,36 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    // user: '',
+    // password: '',
+    database: 'sakila',
+    // waitForConnections: true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+module.exports = app;
+// module.exports.pool = pool;
+
+app.use(function(req, res, next) {
+    req.pool = pool;
+    next();
+});
+
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
+
+
 
 var app = express();
 
