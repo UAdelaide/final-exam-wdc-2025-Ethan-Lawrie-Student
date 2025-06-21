982fd4f6145eca4e2e33fda751f6742d28203ab5
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-21 06:38:32 +0000

    [automatic commit] 2025-06-21 06:38:32 UTC 122.199.51.39

--- part1/queries.sql
+++ part1/queries.sql
@@ -44,4 +44,4 @@ INSERT INTO Dogs (owner_id, name, size) (SELECT user_id , "Fred", 'large' FROM U
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) (SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open' FROM Dogs WHERE name = 'Max');
 
 
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) (SELECT dog_id, '2025-06-10 09:30:00', 30, 'Parklands', 'open' FROM Dogs WHERE name = 'Bella');
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) (SELECT dog_id, '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' FROM Dogs WHERE name = 'Bella');
