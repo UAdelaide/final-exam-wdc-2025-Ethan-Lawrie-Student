21b35431b02fbbf19ffc080a10b247e1c2cf3ba7
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 05:36:35 +0000

    [automatic commit] 2025-06-23 05:36:35 UTC 122.199.51.39

--- part2/routes/loginRoutes.js
+++ part2/routes/loginRoutes.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../models/db');
+// const express = require('express');
+// const router = express.Router();
+// const db = require('../models/db');
 
 
-router.post('/', async (req, res) => {
-    const {username, password} = req.body;
+// router.post('/', async (req, res) => {
+//     const {username, password} = req.body;
 
-  try {
-    const [rows] = await db.query(`
-      SELECT user_id, role FROM Users WHERE username = ? AND password_hash = ?
-    `, [username, password]);
+//   try {
+//     const [rows] = await db.query(`
+//       SELECT user_id, role FROM Users WHERE username = ? AND password_hash = ?
+//     `, [username, password]);
 
-    if(!rows) {
-        res.status(404).json({ error: 'No user' });
-    }
-    const user = rows[0];
-    
-    req.session.role = user.role;
+//     if(!rows) {
+//         res.status(404).json({ error: 'No user' });
+//     }
+//     const user = rows[0];
 
-    if(user.role === "walker") {
+//     req.session.role = user.role;
 
-    }
+//     if(user.role === "walker") {
 
-  } catch (error) {
-    console.error('SQL Error:', error);
-    res.status(500).json({ error: 'Failed to fetch walk requests' });
-  }
-});
+//     }
+
+//   } catch (error) {
+//     console.error('SQL Error:', error);
+//     res.status(500).json({ error: 'Failed to fetch walk requests' });
+//   }
+// });
