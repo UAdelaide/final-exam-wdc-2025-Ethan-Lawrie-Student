3d9ff8a89504b81a91cf80f5cb2a58a9f8c76d73
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 05:53:17 +0000

    [automatic commit] 2025-06-23 05:53:17 UTC 122.199.51.39

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    req.session.user_id = user.user_id;
+    req.session.user_id = rows[0].user_id;
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
