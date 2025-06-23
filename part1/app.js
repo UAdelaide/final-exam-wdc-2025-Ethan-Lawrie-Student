9051da1634d753fb36814f12bbe1aa2343d0ef2c
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 04:17:32 +0000

    [automatic commit] 2025-06-23 04:17:32 UTC 122.199.51.39

--- part1/app.js
+++ part1/app.js
@@ -158,6 +158,7 @@ let db;
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) (SELECT dog_id, '2025-06-11 08:35:01', 35, 'Rundle Mall Start', 'open' FROM Dogs WHERE name = 'Fred');
 
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments, rated_at) VALUES (4, 2, 4, 5, 'was a good walk', '2025-06-23 04:06:25');
+        INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments, rated_at) VALUES (4, 2, 4, 3, 'was a good walk', '2025-06-23 04:06:25');
         `);
 
   } catch (err) {
