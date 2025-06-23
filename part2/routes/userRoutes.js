b3025f0d8cb9763c33682fbbdcf076d5f23b71bd
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 07:42:50 +0000

    [automatic commit] 2025-06-23 07:42:50 UTC 122.199.51.39

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -25,7 +25,7 @@ router.get('/getAllDogs', async (req, res) => {
 
 router.get('/fetchDogInfo', async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT Dogs.dog_id, Dogs.name FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE Dogs.owner_id = ?', [req.session.user_id]);
+    const [rows] = await db.query('SELECT * FROM Dogs');
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
