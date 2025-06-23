0f9a8f5c72776717966b993ea3dd1f726b3feea4
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 04:38:15 +0000

    [automatic commit] 2025-06-23 04:38:14 UTC 122.199.51.39

--- part1/public/javascripts/dogPage.js
+++ part1/public/javascripts/dogPage.js
@@ -8,7 +8,7 @@ const { createApp, ref } = Vue
       function getDog(){
         fetch("https://dog.ceo/api/breeds/image/random").then((data) => {
             if(!data.ok) {
-                Throw
+                throw new Error("")
                 return;
             }
             return data.json();
