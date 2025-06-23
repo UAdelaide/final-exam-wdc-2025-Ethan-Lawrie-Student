ccb45a5cb7d2aff0e2176de340a04fd40ca014dd
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 07:50:38 +0000

    [automatic commit] 2025-06-23 07:50:38 UTC 122.199.51.39

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -26,7 +26,11 @@ router.get('/getAllDogs', async (req, res) => {
 router.get('/fetchDogInfo', async (req, res) => {
   try {
     const [rows] = await db.query('SELECT * FROM Dogs');
-    rows.for
+    rows.forEach(row => {
+      map((row) => {
+        return {...row, await fetch('https://dog.ceo/api/breeds/image/random')}
+      })
+    });
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
