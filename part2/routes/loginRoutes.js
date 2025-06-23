1146919d77d2bd6929fc8a2b41b3755ea366141e
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 05:27:29 +0000

    [automatic commit] 2025-06-23 05:27:28 UTC 122.199.51.39

--- part2/routes/loginRoutes.js
+++ part2/routes/loginRoutes.js
@@ -7,14 +7,9 @@ router.post('/', async (req, res) => {
     const {username, password} = req.body;
 
   try {
-    const [rows] = await db.query(`
-      SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
-      FROM WalkRequests wr
-      JOIN Dogs d ON wr.dog_id = d.dog_id
-      JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open'
+    await db.query(`
+      SELECT 
     `);
-    res.json(rows);
   } catch (error) {
     console.error('SQL Error:', error);
     res.status(500).json({ error: 'Failed to fetch walk requests' });
