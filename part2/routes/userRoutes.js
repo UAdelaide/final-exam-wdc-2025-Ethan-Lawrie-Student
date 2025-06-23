b0c51c5d4770d034b4d732743700fbdebe9cf88f
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 07:23:58 +0000

    [automatic commit] 2025-06-23 07:23:57 UTC 122.199.51.39

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -43,6 +43,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user_id) {
     return res.status(401).json({ error: 'Not logged in' });
   }
+  // console.log("sending: ",)
   res.json({user_id:req.session.user_id});
 });
 
