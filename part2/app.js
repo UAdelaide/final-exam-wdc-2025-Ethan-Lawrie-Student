1a406ade3018f3fda05aaa84aec04c8ed975d741
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 05:36:55 +0000

    [automatic commit] 2025-06-23 05:36:55 UTC 122.199.51.39

--- part2/app.js
+++ part2/app.js
@@ -18,12 +18,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const userRoutes = require('./routes/loginRoutes');
+// const userRoutes = require('./routes/loginRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-app.use('/api/login', loginRoutes);
+// app.use('/api/login', loginRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
