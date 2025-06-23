bf659d314c5478488a258ba21c3a34baa6da19e7
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-06-23 07:29:25 +0000

    [automatic commit] 2025-06-23 07:29:25 UTC 122.199.51.39

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,8 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 60000
+        maxAge: 60000,
+        sameSite: 'lax'
     }
 }))
 // Middleware
